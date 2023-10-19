fe27c75f1faaa0647cdf31ca226be8a6257780f0
ROLOLOL <127593514+rolance110@users.noreply.github.com>
2023-10-19 17:19:18 +0800

    main

--- VGG_practice.py
+++ VGG_practice.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
 
 
 def VGG16(input_shape=(224, 224, 3), num_classes=1000):
@@ -12,7 +14,7 @@ def VGG16(input_shape=(224, 224, 3), num_classes=1000):
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     # Block 2
-    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.amodeldd(Conv2D(128, (3, 3), activation='relu', padding='same'))
     model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
@@ -40,4 +42,33 @@ def VGG16(input_shape=(224, 224, 3), num_classes=1000):
     model.add(Dense(4096, activation='relu'))
     model.add(Dense(num_classes, activation='softmax'))
 
-    return model
+    return
+
+    def main():
+        # Load the MNIST dataset
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+        # Preprocess the data
+        x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+        x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+        x_train = x_train.astype('float32')
+        x_test = x_test.astype('float32')
+        x_train /= 255
+        x_test /= 255
+        y_train = to_categorical(y_train, 10)
+        y_test = to_categorical(y_test, 10)
+
+        # Train the model
+        model = VGG16(input_shape=(28, 28, 1), num_classes=10)
+        model.compile(optimizer='adam',
+                      loss='categorical_crossentropy', metrics=['accuracy'])
+        model.fit(x_train, y_train, batch_size=128,
+                  epochs=10, validation_data=(x_test, y_test))
+
+        # Evaluate the model
+        score = model.evaluate(x_test, y_test, verbose=0)
+        print('Test loss:', score[0])
+        print('Test accuracy:', score[1])
+
+    if __name__ == '__main__':
+        main()
